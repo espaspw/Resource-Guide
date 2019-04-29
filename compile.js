09f091bd9f8f1ca8879346b5b5205007a594626f
Super Panama World <superpanamaworld@gmail.com>
2019-04-28 17:18:28 -0700

    Fixed compile.js to work with new styling

--- compile.js
+++ compile.js
@@ -23,25 +23,36 @@ const strings = `<!DOCTYPE html>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
-  <style>
-${style}
-  </style>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
 <body>
-  <nav><table><tr>
-    <td><a href="#All">All</a></td>
-    <td><a href="#Cantonese">Cantonese</a></td>
-    <td><a href="#Mandarin">Mandarin</a></td>
-    <td><a href="#Other">Other</a></td>
-    <td><a href="#English">English</a></td>${
-  Eager(tags).sieve(isNotInCommon).map(label => `
-    <td><a href="#${label}">${label}</a></td>`
-  ).val().join('')}
-  </tr></table></nav>
+<div class="splitview">
+
+  <nav>
+    <div class="navbar__drop-menu" onclick="">Sections</div>
+    <div class="navbar__container">
+      <a href="#All" id="navbar__main-btn">All</a></td>
+      <a href="#Cantonese">Cantonese</a>
+      <a href="#Mandarin">Mandarin</a>
+      <a href="#Other">Other</a>
+      <a href="#English">English</a>
+      ${Eager(tags).sieve(isNotInCommon).map(label => `<a href="#${label}">${label}</a>`).val().join('')}
+    </div>
+    <div class="navbar__drop-menu__flexbox">
+      <div class="navbar__drop-menu__container">
+          <a href="#All" id="navbar__main-btn">All</a></td>
+          <a href="#Cantonese">Cantonese</a>
+          <a href="#Mandarin">Mandarin</a>
+          <a href="#Other">Other</a>
+          <a href="#English">English</a>
+          ${Eager(tags).sieve(isNotInCommon).map(label => `<a href="#${label}">${label}</a>`).val().join('')}
+        </div>
+    </div>
+  </nav>
   
-
-  <div class="space-for-nav">_</div>
-
+  <div class="responsive-spacer"></div>
+  <div class="main-container">
   <main>${
   tags.map(label => `
     <section id="${label}">${
@@ -55,7 +66,20 @@ ${style}
     </section>
 ` ).join('')}
   </main>
+  </div>
+</div>
 </body>
+<script>
+  // Redirects to #All tag on page load as text is hidden on default index
+  window.onload = () => {
+    window.location.href = "#All";
+    window.scrollTo(0,0);
+  }
+  // Scrolls to top after using fragment ID
+  window.onhashchange = () => {
+    window.scrollTo(0,0);
+  }
+</script>
 </html>`;
 
 console.log(strings);
